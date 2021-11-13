3f7b83225606d2dfb027b64e612841e9a5c36c0c
Pavithra Nair <pmpavithranair@gmail.com>
2021-11-13 19:28:29 +0530

    captured user input

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,17 +1,22 @@
+import { useState } from "react/cjs/react.development"
 import "./NewEventForm.css"
 
 export default function NewEventForm(){
+    const[title, setTitle] = useState('')
+    const[date, setDate] = useState('')
+    
     return(
         <form className="new-event-form"> 
             <label>
                 <span>Event title: </span>
-                <input type = "text" />
+                <input type = "text" onChange= {(e) => setTitle(e.target.value)}/>
             </label>
             <label>
                 <span>Event date: </span>
-                <input type = "date" />
+                <input type = "date" onChange= {(e) => setDate(e.target.value)}/>
             </label>
             <button>Submit</button>
+            <p>title - {title}, date - {date}</p>
         </form>
     )
 }
